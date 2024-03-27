362cfb21a3a789fc1f28565c4643891e9019a2e7
Ulugbek <ulugb3k270@gmail.com>
2024-03-27 12:40:50 +0500

    disabled car wash additional services

--- src/components/car-wash/services/car-wash-types/car-wash-additional-services.tsx
+++ src/components/car-wash/services/car-wash-types/car-wash-additional-services.tsx
@@ -1,43 +1,43 @@
-"use client";
-
-import { CarWashTypeInlineCard } from "@/components/car-wash/services/car-wash-types/car-wash-type-inline-card";
-import { useLocaleContext } from "@/context/locale.context";
-import { useState } from "react";
-
-export const CarWashAdditionalServices = () => {
-  const { t } = useLocaleContext();
-  const [activeService, setActiveService] = useState("1");
-  return (
-    <div>
-      <h2 className="text-lg font-bold text-black text-center py-3 ">
-        {t("services.choose_additional_service")}
-      </h2>
-      <div className="flex flex-col space-y-2">
-        <CarWashTypeInlineCard
-          onChange={setActiveService}
-          isActive={activeService === "1"}
-          icon=""
-          price="150.000 sum"
-          serviceName="2-х фазная мойкa"
-          id="1"
-        />
-        <CarWashTypeInlineCard
-          onChange={setActiveService}
-          isActive={activeService === "2"}
-          icon=""
-          price="150.000 sum"
-          serviceName="2-х фазная мойкa"
-          id="2"
-        />
-        <CarWashTypeInlineCard
-          onChange={setActiveService}
-          isActive={activeService === "3"}
-          icon=""
-          price="150.000 sum"
-          serviceName="2-х фазная мойкa"
-          id="3"
-        />
-      </div>
-    </div>
-  );
-};
+// "use client";
+//
+// import { CarWashTypeInlineCard } from "@/components/car-wash/services/car-wash-types/car-wash-type-inline-card";
+// import { useLocaleContext } from "@/context/locale.context";
+// import { useState } from "react";
+//
+// export const CarWashAdditionalServices = () => {
+//   const { t } = useLocaleContext();
+//   const [activeService, setActiveService] = useState("1");
+//   return (
+//     <div>
+//       <h2 className="text-lg font-bold text-black text-center py-3 ">
+//         {t("services.choose_additional_service")}
+//       </h2>
+//       <div className="flex flex-col space-y-2">
+//         <CarWashTypeInlineCard
+//           onChange={setActiveService}
+//           isActive={activeService === "1"}
+//           icon=""
+//           price="150.000 sum"
+//           serviceName="2-х фазная мойкa"
+//           id="1"
+//         />
+//         <CarWashTypeInlineCard
+//           onChange={setActiveService}
+//           isActive={activeService === "2"}
+//           icon=""
+//           price="150.000 sum"
+//           serviceName="2-х фазная мойкa"
+//           id="2"
+//         />
+//         <CarWashTypeInlineCard
+//           onChange={setActiveService}
+//           isActive={activeService === "3"}
+//           icon=""
+//           price="150.000 sum"
+//           serviceName="2-х фазная мойкa"
+//           id="3"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
